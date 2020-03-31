a16133eaf93c910a64ee0eb251bc8961ec764040
Sean Quinn <s_quinn@bellsouth.net>
2020-03-31 19:35:21 -0400

    Finished week 2 stuff: able to take pictures and save posts

--- app/src/main/java/net/dementia13/srq_instagram/MainActivity.java
+++ app/src/main/java/net/dementia13/srq_instagram/MainActivity.java
@@ -1,6 +1,12 @@
 package net.dementia13.srq_instagram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -8,23 +14,30 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static  final String TAG = "MainActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     private EditText etDescription;
     private Button btnCaptureImage;
     private ImageView ivPostImage;
     private Button btnSubmit;
+    private File photoFile;
+    private String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -36,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
         ivPostImage = findViewById(R.id.ivPostImage);
         btnSubmit = findViewById(R.id.btnSubmit);
 
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+
 //        queryPosts();
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -46,19 +66,72 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Description cannot be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if(photoFile == null || ivPostImage.getDrawable() == null){
+                    Toast.makeText(MainActivity.this, "There is no image!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(description, currentUser);
+                savePost(description, currentUser, photoFile);
             }
         });
     }
 
+    private void launchCamera() {
+        // Create Intent to launch camera and return control to calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // create a file reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
 
+        // wrap file object into a content provider
+        // required for API >= 24
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "net.dementia13.fileprovider", photoFile);
+        // com.codepath.fileprovider
+        //net.dementia13.srq_instagram
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
+        // If no app can handle this intent, you will crash
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
 
-    private void savePost(String description, ParseUser currentUser) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivPostImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // returns the file for a photo stored on disk given the filename
+    public File getPhotoFileUri(String fileName){
+        // get safe storage directory for photos
+        // use 'getExternalFilesDir' on Context to access package-specific directories
+        // avoids need to request external r/w permissions
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // create the storage directory if not exist
+        if(!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
         Post post = new Post();
         post.setDescription(description);
-       // post.setImage();
+        post.setImage(new ParseFile(photoFile));
         post.setUser(currentUser);
         post.saveInBackground(new SaveCallback() {
             @Override
@@ -69,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Log.i(TAG, "Post save was successful!!");
                 etDescription.setText("");
+                ivPostImage.setImageResource(0);
             }
         });
     }
@@ -89,5 +163,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
